43ec9c5ca58069d2dcef9db89b9d9a960e6b2087
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-06-27 22:18:52 +0300

    Add project files.

---
+++ Assignment_C#1.1/Program.cs
@@ -0,0 +1,94 @@
+﻿ namespace Assignment_C_1
+{
+
+    //class player
+    //{
+    //    public string name;
+    //}
+    internal class Program
+    {
+        static void Main()
+        {
+            #region point_1
+            //Console.WriteLine("Enter a Number : ");
+            //string input = Console.ReadLine();
+            //int Num = int.Parse(input);
+            //Console.WriteLine(" Number =" + Num);
+            #endregion
+
+
+            #region point_2
+            //string input = "755mo";
+            //int res = int.Parse(input);
+            //Console.WriteLine(res);
+
+            // throw format exception because the input contain "755mo" non numeric characters
+            #endregion
+
+
+            #region point_3
+            //float num1 = 0.7f;
+            //float num2 = 0.5f;
+
+            //float sum = num1+ num2;
+            //Console.WriteLine(sum);
+            #endregion
+
+
+            #region point_4
+            //string input = "hello world";
+            //Console.WriteLine(input);
+            //string sub = input.Substring(3,7);
+            //Console.WriteLine(sub);
+            #endregion
+
+
+            #region point_5
+            //int num1 = 40;
+            //int num2 = num1;
+
+            //num2 = 70;
+
+            //Console.WriteLine( "Number 1 = "+num1);
+            //Console.WriteLine("number 2 = "+num2);
+
+
+            // when we assign new value for num2 stored this value 
+            #endregion
+
+
+            #region point_6
+
+            //player p1 = new player();
+
+            //p1.name = "mo";
+
+            //player p2 = p1;
+            //p2.name = "cr";
+
+            //Console.WriteLine(p1.name);
+            //Console.WriteLine(p2.name);
+
+
+
+            //when we assign p1 with p2,
+            //p1 and p2 both refer to the same object in memory.
+            //p1 and p2 contain the same value
+            #endregion
+
+
+            #region point_7
+            //string fname = "mohamed";
+            //string lname = "eldeghiedy";
+
+            //string full_name = fname +" "+lname;
+
+            //Console.WriteLine(full_name);
+            #endregion
+
+            // point_8 = b 
+            // point_9 = d
+            // point_10 = d
+        }
+    }
+}
